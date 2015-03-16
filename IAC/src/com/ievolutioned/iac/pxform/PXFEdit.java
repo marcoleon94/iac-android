c0b8d1f362f94a96f7454129efc9521b11a9da2e
Pablo Mendoza A <pablo@ievolutioned.com>
2015-03-16 11:48:29 -0700

    added loading screen
    PXFEdit.java added numeric EditText and text limit size

--- IAC/src/com/ievolutioned/iac/pxform/PXFEdit.java
+++ IAC/src/com/ievolutioned/iac/pxform/PXFEdit.java
@@ -4,6 +4,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import android.content.Context;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -17,29 +19,31 @@ public class PXFEdit extends PXWidget{
 	public PXFEdit(Context context, Map<String, Entry<String, JsonElement>> entry) {
 		super(context, entry);
 	}
-	
+
 	protected View addControls(Context context, 
 			Map<String, Map.Entry<String,JsonElement>> map){
 
 		//layout container
 		LinearLayout linear = getGenericLinearLayout(context);
 		linear.setWeightSum(1);
-		
+
 		//field name
 		TextView text = getGenericTextView(context, 
 				map.containsKey(FIELD_TITLE) ? map.get(FIELD_TITLE).getValue().getAsString() : " ");		
 		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
 				ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 0.5f);
 		text.setLayoutParams(params);
-		
+
 		//edit input control
 		EditText edit = new EditText(context);
 		params = new LinearLayout.LayoutParams(
 				ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 0.5f);
 		edit.setLayoutParams(params);
 		edit.setLines(1);
+		editConfigureTypeOfInput(edit, map);		
+
 		//edit.setMaxLines(1);
-		
+
 		if(map.containsKey(FIELD_PLACEHOLDER)){
 			edit.setHint(map.get(FIELD_PLACEHOLDER).getValue().getAsString());
 		}
@@ -47,7 +51,30 @@ public class PXFEdit extends PXWidget{
 		//add views to the container
 		linear.addView(text);
 		linear.addView(edit);
-		
+
 		return linear;
+	}
+
+	private void editConfigureTypeOfInput(EditText edit, 
+			Map<String, Map.Entry<String,JsonElement>> map){
+		int maxsize = 50;
+
+		if(FIELD_TYPE_LONGTEXT.equals(map.get(FIELD_TYPE).getValue().getAsString())){
+			try{
+				maxsize = Integer.parseInt(map.get(FIELD_TYPE).getValue().getAsString());
+			}catch(Exception ex){
+			}
+		}
+
+		InputFilter[] FilterArray = new InputFilter[1];
+		FilterArray[0] = new InputFilter.LengthFilter(maxsize);
+
+		if(FIELD_TYPE_UNSIGNED.equals(map.get(FIELD_TYPE).getValue().getAsString())){
+			edit.setInputType(InputType.TYPE_CLASS_NUMBER);
+		}else{
+			edit.setInputType(InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE);
+		}
+
+		edit.setFilters(FilterArray);
 	}
 }
