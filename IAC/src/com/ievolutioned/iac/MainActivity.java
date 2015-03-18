8cb063784e2df47c9f3cad709a3014eb3d4aaf5a
Pablo Mendoza A <pablo@ievolutioned.com>
2015-03-18 13:37:54 -0700

    Cleanup code

--- IAC/src/com/ievolutioned/iac/MainActivity.java
+++ IAC/src/com/ievolutioned/iac/MainActivity.java
@@ -12,27 +12,9 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//Crashlytics.start(this);
+		Crashlytics.start(this);
 		setContentView(R.layout.activity_main);
-
-		findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(getApplicationContext(),
-						MFormGenerator.class));
-			}
-		});
-		findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(getApplicationContext(),
-						JsonToView.class));
-			}
-		});
 		findViewById(R.id.button3).setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
 				startActivity(new Intent(getApplicationContext(),
