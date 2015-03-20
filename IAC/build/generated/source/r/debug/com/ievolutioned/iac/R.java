061917e66bfbc0b2a9f42a48a591d834092a48f2
PabloMendozaA <pablo@ievolutioned.com>
2015-03-19 19:58:58 -0700

    a borrar

--- IAC/build/generated/source/r/debug/com/ievolutioned/iac/R.java
+++ IAC/build/generated/source/r/debug/com/ievolutioned/iac/R.java
@@ -49,9 +49,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060000;
         public static final int app_name=0x7f060001;
-        public static final int com_crashlytics_android_build_id=0x7f060002;
-        public static final int hello_world=0x7f060003;
-        public static final int title_activity_pxform=0x7f060004;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_pxform=0x7f060003;
     }
     public static final class style {
         /** 
