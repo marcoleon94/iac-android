e0190a31282e3e803c259ef1e128433f2e70e3f0
Pedro Daniel GG <pedro@ievolutioned.com>
2015-03-23 16:20:34 -0600

    removed android support v4 lib file

--- IAC/src/com/ievolutioned/iac/MainActivity.java
+++ IAC/src/com/ievolutioned/iac/MainActivity.java
@@ -1,25 +1,27 @@
 package com.ievolutioned.iac;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.internal.aD;
 import com.ievolutioned.iac.adapter.MenuDrawerListAdapter;
 import com.ievolutioned.iac.view.MenuDrawerItem;
 
 import java.util.ArrayList;
 
+
 public class MainActivity extends Activity {
 
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerListForm;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     private String[] menuFormTitles;
     private String[] menuSitesTitles;
@@ -42,6 +44,7 @@ public class MainActivity extends Activity {
         // find view
         mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer);
         mDrawerListForm = (ListView) findViewById(R.id.activity_main_menu_forms);
+
         // menu items
 
         menuFormTitles = getResources().getStringArray(R.array.nav_drawer_form_items);
@@ -59,15 +62,66 @@ public class MainActivity extends Activity {
 
         mDrawerListForm.setOnItemClickListener(drawer_click_form);
 
+        setDrawer();
+
     }
+
+    private void setDrawer() {
+
+        final CharSequence mTitle, mDrawerTitle;
+        mTitle = mDrawerTitle = getTitle();
+
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.nav_drawer_open, R.string.nav_drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     AdapterView.OnItemClickListener drawer_click_form = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            
+
         }
     };
-
 
 
     @Override
