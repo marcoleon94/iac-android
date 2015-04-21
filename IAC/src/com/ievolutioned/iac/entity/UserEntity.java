f09e4e8318617a3e27b269eba9696784a68d8f85
Pedro Daniel GG <pedro@ievolutioned.com>
2015-04-21 12:26:03 -0500

    Added user entity new file:   IAC/src/com/ievolutioned/iac/entity/UserEntity.java

---
+++ IAC/src/com/ievolutioned/iac/entity/UserEntity.java
@@ -0,0 +1,36 @@
+package com.ievolutioned.iac.entity;
+
+/**
+ * User entity class. Determines the attributes to consider for a single User in the system
+ *
+ * Created by Daniel on 21/04/2015.
+ */
+public class UserEntity {
+    private String status;
+    private String adminToken;
+    private String adminEmail;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAdminToken() {
+        return adminToken;
+    }
+
+    public void setAdminToken(String adminToken) {
+        this.adminToken = adminToken;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
+}
