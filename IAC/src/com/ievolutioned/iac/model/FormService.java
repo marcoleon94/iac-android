de11373f9fd0459d0f7697400ae8bc547b1e5ee6
Pedro Daniel GG <pedro@ievolutioned.com>
2015-04-22 16:16:56 -0500

    Added javadoc modified:   IAC/src/com/ievolutioned/iac/model/FormService.java

--- IAC/src/com/ievolutioned/iac/model/FormService.java
+++ IAC/src/com/ievolutioned/iac/model/FormService.java
@@ -18,27 +18,66 @@ import java.util.Date;
  * Created by Daniel on 22/04/2015.
  */
 public class FormService {
+    /**
+     * URL for form or inquests on the server
+     */
     private static final String URL_FORM = "https://iacgroup.herokuapp.com/api/inquests/";
 
+    /**
+     * JSON array key for all inquests
+     */
     private static final String JSON_INQUESTS = "inquests";
 
+    /**
+     * JSON key for single inquest
+     */
     private static final String JSON_INQUEST = "inquest";
 
+    /**
+     * Controller constant
+     */
     private static final String CONTROLLER = "inquests";
+
+    /**
+     * Action for get all inquest
+     */
     private static final String ACTION_INDEX = "index";
+    /**
+     * Action for get a single inquest
+     */
     private static final String ACTION_SHOW = "show";
 
+    /**
+     * AsyncTask task for service
+     */
     private AsyncTask<Void, Void, FormResponse> task;
 
+    /**
+     * Required as the unique ID of device for secret
+     */
     private String deviceId = null;
 
+    /**
+     * Required admin token to access on system
+     */
     private String adminToken = null;
 
+    /**
+     * Instantiates a Form service object with current parameters
+     *
+     * @param deviceId   - the UUID of the device
+     * @param adminToken - the user admin token
+     */
     public FormService(String deviceId, String adminToken) {
         this.deviceId = deviceId;
         this.adminToken = adminToken;
     }
 
+    /**
+     * Gets all the forms
+     *
+     * @param callback - ServiceHandler callback
+     */
     public void getForms(final ServiceHandler callback) {
         task = new AsyncTask<Void, Void, FormResponse>() {
             @Override
@@ -52,8 +91,10 @@ public class FormService {
                     }
                     HttpGetParam params = new HttpGetParam();
 
+                    //Get headers
                     HttpHeader headers = getFormHeaders(ACTION_INDEX);
 
+                    // Get response
                     String response = NetUtil.get(URL_FORM, params, headers);
 
                     if (response != null) {
@@ -81,7 +122,13 @@ public class FormService {
         task.execute();
     }
 
-    public void getFormById(final int idForm, final ServiceHandler callback){
+    /**
+     * Gets a form by its id
+     *
+     * @param idForm   - the form id
+     * @param callback - ServiceHandler callback
+     */
+    public void getFormById(final int idForm, final ServiceHandler callback) {
         task = new AsyncTask<Void, Void, FormResponse>() {
             @Override
             protected FormResponse doInBackground(Void... p) {
@@ -93,8 +140,10 @@ public class FormService {
                         this.cancel(true);
                     }
 
+                    //Get headers
                     HttpHeader headers = getFormHeaders(ACTION_SHOW);
-
+                    
+                    // Get response
                     String response = NetUtil.get(URL_FORM + idForm, null, headers);
 
                     if (response != null) {
@@ -122,6 +171,12 @@ public class FormService {
         task.execute();
     }
 
+    /**
+     * Gets the form headers for request
+     *
+     * @param action - the action to perform
+     * @return a HttpHeader
+     */
     private HttpHeader getFormHeaders(String action) {
         HttpHeader headers = new HttpHeader();
 
@@ -149,6 +204,12 @@ public class FormService {
         return headers;
     }
 
+    /**
+     * Handles the result on callback
+     *
+     * @param callback - the callback of service
+     * @param response - the response of service
+     */
     protected void hanldeResult(final ServiceHandler callback, final FormResponse response) {
         if (response == null)
             callback.onError(new FormResponse(null, "Service error", new RuntimeException()));
@@ -170,9 +231,22 @@ public class FormService {
     }
 
 
+    /**
+     * Form response class. Manages the JSON elements of this response
+     */
     public class FormResponse extends ResponseBase {
+        /**
+         * Contains the complete JSON element on response
+         */
         public JsonElement json;
 
+        /**
+         * Instantiates a FormResponse object
+         *
+         * @param json - the JsonElement
+         * @param msg  - the message
+         * @param e    - the error
+         */
         public FormResponse(JsonElement json, String msg, Throwable e) {
             this.json = json;
             this.msg = msg;
