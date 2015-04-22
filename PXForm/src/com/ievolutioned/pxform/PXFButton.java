a279b92b23fa572016649f341b89b5cbbc8169e2
Pablo Mendoza <pablo@ievolutioned.com>
2015-04-21 23:36:55 -0500

    AndroidManifest.xml update to API 22

--- PXForm/src/com/ievolutioned/pxform/PXFButton.java
+++ PXForm/src/com/ievolutioned/pxform/PXFButton.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
