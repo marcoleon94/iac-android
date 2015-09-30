f57c5af40f920454e96e3b800d9e666110aae471
Pedro Daniel GG <pedro@ievolutioned.com>
2015-09-30 17:20:29 -0500

    Added string for view utility get loading modified:   IAC/res/layout/loading.xml modified:   IAC/res/values/strings.xml modified:   IAC/src/com/ievolutioned/iac/view/ViewUtility.java

--- IAC/src/com/ievolutioned/iac/view/ViewUtility.java
+++ IAC/src/com/ievolutioned/iac/view/ViewUtility.java
@@ -5,11 +5,38 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ievolutioned.iac.R;
 
 @SuppressLint("InflateParams")
 public class ViewUtility {
+    /**
+     * Create a loading screen, using an {@link AlertDialog} class
+     *
+     * @param context the context the control will be shown
+     * @param title   the context the control will be shown
+     * @return AlertDialog with an animation
+     * @see {link http://developer.android.com/guide/topics/ui/dialogs.html}
+     * <br />
+     * Avoid using Dialog class directly. If a more complex dialog is needed use
+     * @see {link http://developer.android.com/guide/topics/ui/dialogs.html#FullscreenDialog}
+     */
+    public static AlertDialog getLoadingScreen(Context context, String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.loading, null);
+        if (title != null) {
+            TextView mTitle = (TextView) view.findViewById(R.id.loading_title);
+            mTitle.setText(title);
+        }
+        builder.setView(view);
+        builder.setCancelable(false);
+        AlertDialog alert = builder.create();
+        alert.getWindow().setBackgroundDrawableResource(R.drawable.ic_mapcross_dummy);
+        return alert;
+    }
+
     /**
      * Create a loading screen, using an {@link AlertDialog} class
      *
@@ -21,14 +48,7 @@ public class ViewUtility {
      * @see {link http://developer.android.com/guide/topics/ui/dialogs.html#FullscreenDialog}
      */
     public static AlertDialog getLoadingScreen(Context context) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.loading, null);
-        builder.setView(view);
-        builder.setCancelable(false);
-        AlertDialog alert = builder.create();
-        alert.getWindow().setBackgroundDrawableResource(R.drawable.ic_mapcross_dummy);
-        return alert;
+        return getLoadingScreen(context, null);
     }
 
 }
