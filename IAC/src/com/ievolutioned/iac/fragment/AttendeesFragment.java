777c829083ffd8eb296184a75cab2d8872f31036
daniel <pedro@ievolutioned.com>
2017-02-22 15:29:36 -0600

    Added main view for attendees

--- IAC/src/com/ievolutioned/iac/fragment/AttendeesFragment.java
+++ IAC/src/com/ievolutioned/iac/fragment/AttendeesFragment.java
@@ -2,7 +2,6 @@ package com.ievolutioned.iac.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +15,14 @@ import com.ievolutioned.iac.R;
 
 public class AttendeesFragment extends BaseFragmentClass {
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View root = inflater.inflate(R.layout.fragment_courses, container, false);
+        View root = inflater.inflate(R.layout.fragment_attendees, container, false);
         setHasOptionsMenu(true);
         bindUI(root);
         bindData(getArguments());
-        setTitle(getString(R.string.string_fragment_courses_title));
+        setTitle(getString(R.string.string_fragment_attendees_title));
         return root;
     }
 
