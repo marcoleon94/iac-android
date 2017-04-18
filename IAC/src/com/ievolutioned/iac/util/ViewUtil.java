a3a9ae84324a9c5fefba21e9527b2e6ad76d9ba7
daniel <pedro@ievolutioned.com>
2017-04-18 17:50:59 -0500

    Fixed listHeight modified:   IAC/src/com/ievolutioned/iac/util/ViewUtil.java

--- IAC/src/com/ievolutioned/iac/util/ViewUtil.java
+++ IAC/src/com/ievolutioned/iac/util/ViewUtil.java
@@ -17,20 +17,17 @@ public class ViewUtil {
             return;
         }
 
-        int totalHeight = listView.getPaddingTop() + listView.getPaddingBottom();
-
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
         for (int i = 0; i < listAdapter.getCount(); i++) {
             View listItem = listAdapter.getView(i, null, listView);
-            if (listItem instanceof ViewGroup) {
-                listItem.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            }
-
-            listItem.measure(0, 0);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
             totalHeight += listItem.getMeasuredHeight();
         }
 
         ViewGroup.LayoutParams params = listView.getLayoutParams();
         params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
         listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
